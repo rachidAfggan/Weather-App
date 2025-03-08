cfc2bc42ef5c7a77c30ef63816e3b76dec237949
rachidAfggan <afggan.rachid@gmail.com>
2025-03-08 00:20:58 +0000

    Add some functionality in JavaScript with weather API

---
+++ script.js
@@ -0,0 +1,20 @@
+const apiKey = "3e0abf9bacb6352aec6fffd7b4001df2";
+
+const apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+
+async function checkWeather(city) {
+  const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+  let data = await response.json();
+
+  document.querySelector(".city").innerHTML = data.name;
+  document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
+  document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+  document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+}
+searchBtn.addEventListener("click", () => {
+  checkWeather(searchBox.value);
+});
